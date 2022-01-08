cb6ee5ef9243faf039dd1788e8f09ab9833b9b79
binhnhu1409 <binhnhu1409@gmail.com>
2022-01-08 18:45:41 +0200

    connect database via sqlite3

--- flaskFarm/__init__.py
+++ flaskFarm/__init__.py
@@ -1,14 +1,30 @@
 import os
-
 from flask import Flask
 
 
 def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
+    app.config["DATABASE"] = os.path.join(
+        app.instance_path, "flaskFarm.sqlite")
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route("/hello")
     def hello():
         return "Hello, World!"
+
+    from flaskFarm import db
+
+    db.init_app(app)
+
+    # get the database
+    with app.app_context():
+        database = db.init_db()
+        print(database)
 
     return app
