2a2aa9f92367ce0e1b498857f02ef55b4866ee6f
binhnhu1409 <binhnhu1409@gmail.com>
2022-01-09 12:18:33 +0200

    reading uploaded csvData successfully

--- flaskFarm/__init__.py
+++ flaskFarm/__init__.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+
+from flaskFarm import get_db
 
 
 def create_app(test_config=None):
@@ -24,7 +27,35 @@ def create_app(test_config=None):
 
     # get the database
     with app.app_context():
-        database = db.init_db()
-        print(database)
+        db.init_db()
+
+    # Get the uploaded files
+
+    @app.route("/upload", methods=["GET", "POST"])
+    def uploadFiles():
+
+        # get the uploaded file
+        if request.method == "POST":
+            uploaded_file = request.files["file"]
+            # set the file path
+            if uploaded_file.filename != '':
+                file_path = os.path.join(
+                    app.instance_path, uploaded_file.filename)
+                # save the file
+                uploaded_file.save(file_path)
+                parseCSV(file_path)
+                print('file path is:', file_path)
+            return redirect("/")
+
+        # when request via GET, display form to upload a file
+        else:
+            return render_template('upload.html')
+
+    def parseCSV(filePath):
+        # CVS Column Names
+        col_names = ["Farm name", "datetime", "metric type", "metric value"]
+        # Use Pandas to parse the CSV file
+        csvData = pd.read_csv(filePath, names=col_names, header=None)
+        print(csvData)
 
     return app
